5e7d9fec291433e72ed82806126712325563a271
junaid <junaid.nom@gmail.com>
2020-09-18 21:54:09 -0400

    got blocked list

--- live_to_want_game/src/lib.rs
+++ live_to_want_game/src/lib.rs
@@ -63,6 +63,89 @@ fn run_frame(mut game_state: GameState, root: &GoalNode) -> GameState {
     }
 
     // TODO: Run spawn systems first, and spawn new creatures
+    let spawn_events: Vec<Option<EventChain>> = m.regions.par_iter().flat_map(|x| {
+        x.par_iter().flat_map(|y| {
+            y.grid.par_iter().flat_map(|xl| {
+                xl.par_iter().flat_map(|yl| {
+                    if let Some(creatures) = yl.creatures.as_ref() {
+                        let ret: Vec<Option<EventChain>> = creatures.par_iter().map(
+                            |c| {
+                                budding_system(&m, c)
+                            }
+                        ).collect();
+                        return ret;
+                    } else {
+                        Vec::new()
+                    }
+                })
+            })
+        })
+    }).collect();
+    let mut event_chains: Vec<EventChain> = spawn_events.into_par_iter().flat_map(
+        |opt| {
+            if let Some(ec) = opt {
+                vec![ec]
+            } else {
+                Vec::new()
+            }
+        }).collect();
+
+    process_events_from_mapstate(&mut m, event_chains);
+
+    // TODO: Deal with blockers that spawned
+    // first vec is BLOCKERS second vec is NON BLOCKERS
+    let blocked_creatures: Vec<(Vec<CreatureState>, Vec<CreatureState>)> = m.regions.par_iter_mut().flat_map(|x| {
+        x.par_iter_mut().flat_map(|y| {
+            y.grid.par_iter_mut().flat_map(|xl| {
+                xl.par_iter_mut().map(|yl| {
+                    if let Some(creatures) = yl.creatures.as_mut() {
+                        let mut ret: (Vec<CreatureState>, Vec<CreatureState>) = (Vec::new(), Vec::new());
+                        // if there is a blocking creature and any other creature here
+                        // then we have to remove them
+                        let mut first_blocker: Option<UID> = None;
+                        for i in 0..creatures.len() {
+                            let c = &creatures[i];
+                            if let Some(_) = c.components.block_space_component {
+                                first_blocker = Some(c.components.id_component.id());
+                                break;
+                            }
+                        };
+                        if let Some(first) = first_blocker {
+                            for i in 0..creatures.len() {
+                                if i < creatures.len() {
+                                    let c = &creatures[i];
+                                    if c.components.id_component.id() != first {
+                                        if let Some(_) = c.components.block_space_component {
+                                            ret.0.push(creatures.remove(i));
+                                        } else {
+                                            ret.1.push(creatures.remove(i));
+                                        }
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                        return ret;
+                    } else {
+                        (Vec::new(), Vec::new())
+                    }
+                })
+            })
+        })
+    }).collect();
+    let (blocked_blockers, blocked_nonblockers): (Vec<CreatureState>, Vec<CreatureState>) = blocked_creatures.into_par_iter().
+    reduce(|| (Vec::new(), Vec::new()),|(mut tl1, mut tl2), (l1, l2)| {
+        tl1.extend(l1.into_iter());
+        tl2.extend(l2.into_iter());
+        (tl1, tl2)
+    });
+    // TODO: go through blocking creatures list first and have them find nearest
+    // non blocking location. then have them each return a new list of Creatures that are newly blocked
+    // gonna be weird doing it in parallel based on region. will have to make a hashmap of
+    // tuples of &mut Region : Vec<creatures>. then run on each (&mut Region, Vec)
+    // probably better off for now just going through linearly
+    
 
     // TODO: update nav map based on spawns for regions that spawn ones that block
 
@@ -131,12 +214,14 @@ fn run_frame(mut game_state: GameState, root: &GoalNode) -> GameState {
             })
         })
     }).collect();
-    let mut event_chains = Vec::new();
-    for o in op_ecs {
-        if let Some(ec) = o {
-            event_chains.push(ec);
-        }
-    }
+    let mut event_chains = op_ecs.into_par_iter().flat_map(
+        |opt| {
+            if let Some(ec) = opt {
+                vec![ec]
+            } else {
+                Vec::new()
+            }
+        }).collect();
 
     process_events_from_mapstate(&mut m, event_chains);
 
