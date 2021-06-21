c1519e3bf39316be669a7d00562483052f325f17
Junaid Nomani <junaid@correlation-one.com>
2021-06-21 15:32:38 -0400

    Most of server done, still need to finish connection manager and test

--- live_to_want_game/src/networking/messages.rs
+++ live_to_want_game/src/networking/messages.rs
@@ -1,9 +1,112 @@
-use crate::{CreatureCommandUser, GameState};
+use std::collections::HashMap;
+
+use crate::{CreatureCommandUser, GameState, UID, create_server};
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum GameMessage {
     StringMsg(String),
     GameStateMsg(GameState),
     CreatureCommandMsg(CreatureCommandUser),
+    LoginMsg{user: User},
+    DropConnection(UID),
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct GameMessageWrap{
+    pub message: GameMessage,
+    pub conn_uid: UID,
+}
+
+#[derive(Debug)]
+pub enum ConnectionMessageWrap {
+    SaveClientConnection(UID, UnboundedSender<GameMessageWrap>),
+    GameMessageWrap(GameMessageWrap)
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct User {
+    pub username: String,
+    pub password: String
+}
+
+pub struct LoginManager {
+    conn_to_user: HashMap<UID, User>,
+    username_to_conn: HashMap<String, UID>,
+    username_to_password: HashMap<String, String>,
+}
+impl LoginManager {
+    pub fn new() -> Self{
+        LoginManager {
+            conn_to_user: HashMap::new(),
+            username_to_conn: HashMap::new(),
+            username_to_password: HashMap::new(),
+        }
+    }
+
+    // If user already logged in, but its valid, drop old connection
+    pub fn login_user(&mut self, to_add: User, conn_id: UID) -> Option<GameMessageWrap> {
+        //TODO: Hash passwords
+        if self.username_to_password.contains_key(&to_add.username) {
+            if self.username_to_password[&to_add.username] != to_add.password {
+                return None;
+            }
+        }
+
+        // if logged in already... disconnect old conn?
+        let ret = if self.username_to_conn.contains_key(&to_add.username) {
+            Some(GameMessageWrap{
+                message: GameMessage::DropConnection(self.username_to_conn[&to_add.username]),
+                conn_uid: self.username_to_conn[&to_add.username],
+            })
+        } else {
+            None
+        };
+        self.username_to_conn.insert(to_add.username.clone(), conn_id);
+        self.conn_to_user.insert(conn_id, to_add);
+        ret
+    }
+
+    pub fn remove_connection(&mut self, conn_id: UID) {
+        let rmed = self.conn_to_user.remove(&conn_id);
+        if let Some(user) = rmed {
+            self.username_to_conn.remove(&user.username);
+        }
+    }
+
+    pub fn get_conn_id(&self, username: &String) -> Option<UID> {
+        self.username_to_conn.get(username).map_or(None, |c| Some(c.clone()))
+    }
+
+    pub fn get_username(&self, conn_id: &UID) -> Option<String> {
+        self.conn_to_user.get(&conn_id).map_or(None, |c| Some(c.username.clone()))
+    }
+}
+
+#[derive(Debug)]
+pub struct ConnectionManager {
+    send_to_clients: UnboundedSender<ConnectionMessageWrap>,
+    receive_server_messages: UnboundedReceiver<GameMessageWrap>,
+}
+impl ConnectionManager {
+    // TODONEXT: on init, creates a server. Then saves the returned channels.
+    // then has methods, receive message and send message.
+    // it should manage username:conn_uid and stuff here?
+    pub async fn new() -> Self {
+        let (send_to_clients, receive_server_messages) = create_server().await;
+        ConnectionManager {
+            send_to_clients,
+            receive_server_messages,
+        }
+    }
+    pub fn send_message(message: GameMessage, username: String) {
+
+    }
+    pub fn get_messages(&self) -> Vec<GameMessage> {
+        // TODONEXT: Read all the current messages on the list.
+        // if any are UserLogin, check if valid, if invalid respond with error?
+        // if any DropConnection messages, handle here, just remove_connection from LoginManager
+        vec![]
+    }
 }
--- live_to_want_game/src/networking/server.rs
+++ live_to_want_game/src/networking/server.rs
@@ -1,69 +1,107 @@
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
-use std::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::mpsc::{self, UnboundedSender, UnboundedReceiver};
 use std::{thread, time};
-use crate::GameMessage;
+use crate::{ConnectionMessageWrap, GameMessage, GameMessageWrap, UID, get_id};
 use tokio::time::{sleep, Duration};
 
 const IP_PORT: &str = "127.0.0.1:7726";
 
 #[tokio::main]
-pub async fn dumb_server() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind(IP_PORT).await?;
-    println!("Started dumb server");
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-                println!("Got data! {:?}", &buf[0..n]);
-                
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
-}
-
-#[tokio::main]
-async fn start_server(sender: Sender<GameMessage>) -> Result<(), Box<dyn std::error::Error>>  {
+async fn start_server(send_to_server: UnboundedSender<GameMessageWrap>, clients_sender: UnboundedSender<ConnectionMessageWrap>, mut clients_receive: UnboundedReceiver<ConnectionMessageWrap>) -> Result<(), Box<dyn std::error::Error>>  {
     let listener = TcpListener::bind(IP_PORT).await?;
     
     println!("Listening on {:?} {:?}", IP_PORT, listener.local_addr());
     let mut started = false;
+    
+    let all_sender_server = send_to_server.clone();
+    tokio::spawn(async move {
+        let mut client_connections: HashMap<UID, UnboundedSender<GameMessageWrap>> = HashMap::new();
+        loop {
+            match clients_receive.recv().await {
+                Some(conn_msg) => {
+                    match conn_msg {
+                        ConnectionMessageWrap::SaveClientConnection(c_uid, sender) => {client_connections.insert(c_uid, sender);},
+                        ConnectionMessageWrap::GameMessageWrap(game_msg_wrap) => {
+                            if client_connections.contains_key(&game_msg_wrap.conn_uid) {
+                                match client_connections[&game_msg_wrap.conn_uid].send(GameMessageWrap{
+                                    conn_uid: game_msg_wrap.conn_uid,
+                                    message: game_msg_wrap.message
+                                }) {
+                                    Ok(_) => {},
+                                    Err(e) => {
+                                        eprintln!("failed to write to client channel (client dc?); err = {:?}", e);
+                                        all_sender_server.send(GameMessageWrap{
+                                            message: GameMessage::DropConnection(game_msg_wrap.conn_uid),
+                                            conn_uid: 0,
+                                        }).unwrap();
+                                        client_connections.remove(&game_msg_wrap.conn_uid);
+                                    },
+                                }
+                            }
+                        },
+                    }
+                },
+                None => {
+                    eprintln!("failed to read to clients receiver!");
+                    return
+                },
+            }
+
+        }
+    });
+
     loop {
         if !started {
-            sender.send(string_to_game_msg("Starting Server!".to_string())).unwrap();
+            send_to_server.send(string_to_game_msg("Starting Server!".to_string(), 0)).unwrap();
             started = true;
             println!("sent started");
         }
-        let (mut socket, _) = listener.accept().await?;
-        let thread_sender = sender.clone();
+        let (socket, _) = listener.accept().await?;
+        let (mut socket_read, mut socket_write) = socket.into_split();
+        let client_uid = get_id();
+        let thread_sender_server = send_to_server.clone();
+        let  (send_to_client, mut receive_client): (UnboundedSender<GameMessageWrap>, UnboundedReceiver<GameMessageWrap>) = mpsc::unbounded_channel();
+        clients_sender.send(ConnectionMessageWrap::SaveClientConnection(client_uid, send_to_client)).unwrap();
+
+        // make one thread for receiving msgs from client, another to send them to the client
+        tokio::spawn(async move {
+            loop {
+                let msg_to_send = receive_client.recv().await;
+                match msg_to_send {
+                    Some(m) => {
+                        if let GameMessage::DropConnection(_) = m.message {
+                            socket_write.shutdown().await.unwrap();
+                            return;
+                        }
+                        let serialized_m = serde_json::to_vec(&m).unwrap();
+                        match socket_write.write(&serialized_m).await {
+                            Ok(_) => {},
+                            Err(e) => {
+                                eprintln!("failed to write to socket; err = {:?}", e);
+                                //socket_write.shutdown().await.unwrap();
+                                // I think shutdown unneccesary because we are dropping the thread here anyway
+                                return;
+                            },
+                        }
+                    },
+                    None => break,
+                }
+            }
+            
+            println!("Turning off client sending {}", client_uid);
+        });
         tokio::spawn(async move {
             let mut buf = [0; 1024];
             
             // In a loop, read data from the socket and write the data back.
             loop {
-                let n = match socket.read(&mut buf).await {
+                let n = match socket_read.read(&mut buf).await {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
@@ -74,59 +112,34 @@ async fn start_server(sender: Sender<GameMessage>) -> Result<(), Box<dyn std::er
                 };
 
                 let msg = &buf[0..n];
-                let message: GameMessage = serde_json::from_slice(msg).unwrap();
-                thread_sender.send(message).unwrap();
+                let message: GameMessageWrap = serde_json::from_slice(msg).unwrap();
 
-                // Write the data back
-                if let Err(e) = socket.write_all(msg).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+                thread_sender_server.send(message).unwrap();
             }
         });
     }
 }
 
-pub fn create_server() -> Receiver<GameMessage> {
+pub async fn create_server() -> (UnboundedSender<ConnectionMessageWrap>, UnboundedReceiver<GameMessageWrap>) {
     // let (sender: Sender<GameMessage>, receiver: Receiver<GameMessage>) = mpsc::channel();
-    let (sender, receiver) = mpsc::channel();
+    let  (sender_to_server, mut receive_server) = mpsc::unbounded_channel();
+    let  (sender_to_clients, receieve_clients) = mpsc::unbounded_channel();
+    let send_clients_clone = sender_to_clients.clone();
     thread::spawn(|| {
-        start_server(sender).unwrap();
+        start_server(sender_to_server, send_clients_clone, receieve_clients).unwrap();
     });
 
-    let start_msg = receiver.recv();
+    let start_msg = receive_server.recv().await;
     println!("Got start msg: {:?}", start_msg.unwrap());
-    receiver
+    (sender_to_clients, receive_server)
 }
 
-pub async fn create_server_dumb() -> Receiver<GameMessage> {
-    // let (sender: Sender<GameMessage>, receiver: Receiver<GameMessage>) = mpsc::channel();
-    let (sender, receiver) = mpsc::channel();
-
-    let handler = thread::spawn(|| {
-        dumb_server().unwrap();
-    });
-    
-    println!("b4 spawn");
-    // let handle = tokio::task::spawn_blocking(|| {
-    //     //dumb_server().unwrap();
-    //     println!("Started other thread");
-    //     for i in 0..10 {
-    //         // sleep(Duration::from_millis(1000));
-    //         thread::sleep(time::Duration::from_millis(1000));
-    //         println!("next {}", i);
-    //     }
-    // });
-    println!("af spawn");
-    //println!("waiting for start msg");
-    //let start_msg = receiver.recv();
-    //println!("Got start msg: {:?}", start_msg.unwrap());
-    receiver
-}
-
-pub fn string_to_game_msg(string_msg: String) -> GameMessage {
-    let msg = GameMessage::StringMsg(string_msg);
-    return msg;
+pub fn string_to_game_msg(string_msg: String, conn_uid: UID) -> GameMessageWrap {
+    let message = GameMessage::StringMsg(string_msg);
+    return GameMessageWrap{
+        message,
+        conn_uid
+    };
 }
 
 pub fn string_to_msg_buffer(string_msg: String) -> Vec<u8> {
@@ -166,4 +179,62 @@ pub fn test_client() {
         }
     }
     println!("Client Terminated.");
+}
+
+#[tokio::main]
+pub async fn dumb_server() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(IP_PORT).await?;
+    println!("Started dumb server");
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+                println!("Got data! {:?}", &buf[0..n]);
+                
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
+
+pub async fn create_server_dumb() -> std::sync::mpsc::Receiver<GameMessage> {
+    // let (sender: Sender<GameMessage>, receiver: Receiver<GameMessage>) = mpsc::channel();
+    let (sender, receiver) = std::sync::mpsc::channel();
+
+    let handler = thread::spawn(|| {
+        dumb_server().unwrap();
+    });
+    
+    println!("b4 spawn");
+    // let handle = tokio::task::spawn_blocking(|| {
+    //     //dumb_server().unwrap();
+    //     println!("Started other thread");
+    //     for i in 0..10 {
+    //         // sleep(Duration::from_millis(1000));
+    //         thread::sleep(time::Duration::from_millis(1000));
+    //         println!("next {}", i);
+    //     }
+    // });
+    println!("af spawn");
+    //println!("waiting for start msg");
+    //let start_msg = receiver.recv();
+    //println!("Got start msg: {:?}", start_msg.unwrap());
+    receiver
 }
