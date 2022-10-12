d2542cb8d33edd1d05a78508f407fc965e99a865
Junaid <junaid.nom@gmail.com>
2022-10-12 00:24:51 -0400

    test kinda works but fails because NAN

--- deep_ai/src/math_rl_game.rs
+++ deep_ai/src/math_rl_game.rs
@@ -28,21 +28,39 @@ impl MathRLGame{
         };
         let mut goal = starting.clone();
         let mut answer = vec![];
+        let length = starting.len();
         for _ in 0..sequence_until_goal {
-            let pos1 = rng.gen_range(1, 5);
-            let mut pos2 = rng.gen_range(1, 5);
+            let pos1 = rng.gen_range(0, length);
+            let mut pos2 = rng.gen_range(0, length);
             if pos2 == pos1 {
-                pos2 = if pos2 == 4 { rng.gen_range(1, 4) } else { pos1 + 1} 
+                pos2 = if pos2 == (length - 1) { 
+                    rng.gen_range(1, pos1) 
+                } else { 
+                    pos1 + 1
+                }
             }
-            
+            assert!(pos1 < length);
+            assert!(pos2 < length);
             assert_ne!(pos1, pos2);
-            let op = MathRLGameMove {
+            let mut op = MathRLGameMove {
                 operation: rng.gen_range(1, 5),
                 pos1,
                 pos2,
                 save_into_pos1: rng.gen_bool(0.5),
                 undone: false,
             };
+            // Don't divide by zero
+            if op.operation == 4 {
+                let divisor = if op.save_into_pos1 { goal[op.pos2] } else { goal[op.pos1] };
+                if divisor == 0. {
+                    op.operation = 1;
+                }
+            }
+            // dont mult by 0, makes it boring
+            if op.operation == 3 && (goal[op.pos1] == 0. || goal[op.pos2] == 0.) {
+                op.operation = 1;
+            }
+
             MathRLGame::operate_inplace(&op, &mut goal, None);
             answer.push(op);
         }
@@ -80,6 +98,12 @@ impl MathRLGame{
 
     pub fn check_if_won(&self) -> bool {
         return self.current == self.goal;
+
+        let mut diff = 0.;
+        for i in 0..self.goal.len() {
+            diff += (self.current[i] - self.goal[i]).abs();
+        }
+        return diff < 0.0001;
     }
 
     pub fn operate(op: &MathRLGameMove, src: &Vec<f32>, previous: Option<&mut MathRLGameMove>) -> Vec<f32> {
@@ -225,10 +249,274 @@ pub fn test_math_rl_game() {
     ];
     for m in moves_to_do {
         many_undos.operate_self(m);
-        println!("game state now: {:#?}", many_undos);
+        //println!("game state now: {:#?}", many_undos);
     }
     assert_eq!(many_undos.current.into_iter().map(|x| (x * 100.).round()).collect::<Vec<f32>>(), vec![2.0f32, 2., 12.].into_iter().map(|x| (x * 100.).round()).collect::<Vec<f32>>());
 
     // make a test where we do a game. try every operation. and make sure u try subtract and divide with diff bools for which one u save to.
+    let mut all_ops = MathRLGame {
+        starting: vec![6., 4., 3.],
+        goal: vec![9., 1., 4.],
+        moves: vec![],
+        current: vec![6., 4., 3.],
+        answer: vec![],
+        won: false,
+    };
+    let moves_for_win = vec![
+        MathRLGameMove { // 6 * 4 = 24, 24, 4, 3
+            operation: 3,
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 4, 3
+            operation: 4,
+            pos1: 2, 
+            pos2: 0, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // undo
+            operation: 0,
+            pos1: 2, 
+            pos2: 0, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 4, 3
+            operation: 4,
+            pos1: 2, 
+            pos2: 0, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 4, 12
+            operation: 3, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 4, 8
+            operation: 2, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 4, 4
+            operation: 2,
+            pos1: 2, 
+            pos2: 1, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 1, 4
+            operation: 4, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // 8, 1, 4
+            operation: 4, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        
+        MathRLGameMove { // 8, 9, 4 (bad shud be undo)
+            operation: 1, 
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: false,
+            undone: false 
+        },
+
+        MathRLGameMove { // idk
+            operation: 4, 
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 4, 
+            pos1: 0, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 3, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 3, 
+            pos1: 2, 
+            pos2: 1, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 3, 
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 3, 
+            pos1: 0, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 1, 
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 4, 
+            pos1: 1, 
+            pos2: 0, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 1, 
+            pos1: 0, 
+            pos2: 1, 
+            save_into_pos1: false,
+            undone: false 
+        },
+        MathRLGameMove { // idk
+            operation: 3, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: false,
+            undone: false 
+        },
+
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+        MathRLGameMove { // UNDO
+            operation: 0, 
+            pos1: 1, 
+            pos2: 2, 
+            save_into_pos1: true,
+            undone: false 
+        },
+
+
+        MathRLGameMove { // 9, 1, 4
+            operation: 1, 
+            pos1: 1, 
+            pos2: 0, 
+            save_into_pos1: false,
+            undone: false 
+        },
+    ];
+    for op in moves_for_win {
+        all_ops.operate_self(op);
+        println!("game state now: {:#?}", all_ops.current);
+    }
+    assert_eq!(all_ops.current, vec![9., 1., 4.]);
+    assert_eq!(all_ops.won, true);
+    // Make a game with a 100 length sequence and check you win if u do all the moves in answer
+}
+
+#[test]
+pub fn run_long_sequence() {
+    let mut long_one = MathRLGame::new(3, 200, 99.);
+    for mov in long_one.answer.clone() {
+        long_one.operate_self(mov);
+        println!("game state now: {:#?}", long_one.current);
+    }
+    println!("game state now: {:#?} goal {:#?}", long_one.current, long_one.goal);
+    assert_eq!(long_one.won, true);
 }
 
